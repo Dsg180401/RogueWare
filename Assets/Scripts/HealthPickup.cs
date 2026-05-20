9dae2f3d52c8b4bacf7f9142a222b78af308c1d1
Dsg180401 <21547274@student.uwl.ac.uk>
2026-05-20 14:42:03 +0100

    script edit

--- Assets/Scripts/HealthPickup.cs
+++ Assets/Scripts/HealthPickup.cs
@@ -7,19 +7,12 @@ public class HealthPickup : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // Check if the player touched the pickup
         if (collision.CompareTag("Player"))
         {
-            // Get the player's HealthController
             HealthController playerHealth = collision.GetComponent<HealthController>();
-
-            // Make sure it exists
             if (playerHealth != null)
             {
-                // Heal the player
-                playerHealth.RecoverHealth(healAmount);
-
-                // Destroy the pickup
+                playerHealth.RecoverHealth(healAmount)
                 Destroy(gameObject);
             }
         }
